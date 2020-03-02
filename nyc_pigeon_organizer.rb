269bc94dfed5ddb2d6cb2b77ade0579c798831d7
Kyle Riner <rinerkyle97@gmail.com>
2020-03-02 04:29:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,6 @@
 def nyc_pigeon_organizer(data)
-  data.map({}) do |pigeon|
-    if pigeon[:gender][:male]
-      pigeon[:gender][:male]
-    else
-      pigeon[:gender][:female]
-    end
+  data.reduce({}) do |memo, |
+    
   end
   p data
 end
