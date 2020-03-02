c1f2ec4c4b41c480bf56024b1c73b37651de9105
Kyle Riner <rinerkyle97@gmail.com>
2020-03-02 04:44:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,22 @@
 def nyc_pigeon_organizer(data)
   pigeons = {}
   
+  data.each { |descriptions, item|
+    item.each { |results, names|
+      names.each { |name|
+      
+        if pigeons[name] == nil
+          pigeons[name] = {}
+        end
+        if pigeons[name][descriptions] == nil
+          pigeons[name][descriptions] = []
+        end
+        
+        results << pigeons[name][descriptions]
+      }
+    }
+  }
   
+  return pigeons
   
 end
